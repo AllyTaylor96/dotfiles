14c3e68c4be8f477ae473da23732cdfea24ead3e
AllyTaylor96 <allytaylor@hotmail.co.uk>
2026-09-17 15:28:21 +0100

    fix treesitter issues

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,11 +1,15 @@
+-- NOTE: nvim-treesitter's `main` branch is a full rewrite that requires
+-- Neovim 0.12+ (nightly) and drops the old incremental_selection/highlight
+-- module system. We're on Neovim 0.11, so we pin the `master` branch, which
+-- is frozen specifically to keep working on Neovim <=0.11.
 return {
   "nvim-treesitter/nvim-treesitter",
-  version = false,
+  branch = "master",
   build = function()
     require("nvim-treesitter.install").update({ with_sync = true })
   end,
   config = function()
-    require("nvim-treesitter.config").setup({
+    require("nvim-treesitter.configs").setup({
       ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "javascript" },
       auto_install = false,
       highlight = { enable = true, additional_vim_regex_highlighting = false },
